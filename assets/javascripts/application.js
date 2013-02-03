448d3b044b88e5c4e0023d8c420a1f6ccecaeaa8
David Sulc <davidsulc@gmail.com>
2013-02-03 17:41:14 +0100

    set composite view's collection in initializer

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -28,6 +28,10 @@ HeroView = Backbone.Marionette.CompositeView.extend({
     'click a': 'logInfoUrl'
   },
   
+  initialize: function(){
+    this.collection = this.model.get('villains');
+  },
+  
   logInfoUrl: function(){
     console.log(this.model.get('info_url'));
   }
