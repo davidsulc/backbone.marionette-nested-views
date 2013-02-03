1e0b62d9dff21db127cd49641a06934714801f0d
David Sulc <davidsulc@gmail.com>
2013-02-03 15:34:12 +0100

    add collection and items views

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -3,3 +3,11 @@ MyApp = new Backbone.Marionette.Application();
 MyApp.addRegions({
   mainRegion: "#content"
 });
+
+HeroView = Backbone.Marionette.ItemView.extend({
+  template: "#accordion-group-template"
+});
+
+AccordionView = Backbone.Marionette.CollectionView.extend({
+  itemView: HeroView
+});
