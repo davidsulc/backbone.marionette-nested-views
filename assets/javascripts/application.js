82e1025b7ff6e894c4cd340a9f53a5e437f7394c
David Sulc <davidsulc@gmail.com>
2013-02-11 21:27:20 +0100

    First pages commit

---
+++ assets/javascripts/application.js
@@ -0,0 +1,60 @@
+MyApp = new Backbone.Marionette.Application();
+
+MyApp.addRegions({
+  mainRegion: "#content"
+});
+
+Villain = Backbone.Model.extend({});
+
+Villains = Backbone.Collection.extend({
+  model: Villain
+});
+
+Hero = Backbone.Model.extend({});
+
+Heroes = Backbone.Collection.extend({
+  model: Hero
+});
+
+VillainView = Backbone.Marionette.ItemView.extend({
+  template: "#villain-template",
+  tagName: "li",
+  
+  events: {
+    'click li': 'logInfoUrl'
+  },
+  
+  logInfoUrl: function(){
+    console.log(this.model.get('info_url'));
+  }
+});
+
+HeroView = Backbone.Marionette.CompositeView.extend({
+  template: "#accordion-group-template",
+  
+  className: "accordion-group",
+  
+  itemView: VillainView,
+  
+  itemViewContainer: "ul",
+  
+  events: {
+    'click a': 'logInfoUrl'
+  },
+  
+  initialize: function(){
+    this.collection = this.model.get('villains');
+  },
+  
+  logInfoUrl: function(){
+    console.log(this.model.get('info_url'));
+  }
+});
+
+AccordionView = Backbone.Marionette.CollectionView.extend({
+  id: "heroList",
+  
+  className: "accordion",
+  
+  itemView: HeroView
+});
